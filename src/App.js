c3f2aa97bc09886f0071519fa76c70e0903a3e34
Ain <ain.salula@gmail.com>
2017-02-14 19:59:11 +0200

    render method

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const App = () => <h1>Hi</h1>
+class App extends React.Component {
+  render() {
+    return <h1>Hello World</h1>
+  }
+}
+
+//const App = () => <h1>Hello stateless</h1>
 
 export default App;
